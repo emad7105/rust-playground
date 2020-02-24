334aee2cc8a1712cc29af1ca12960acb289c7973
Emad Heydari Beni <emad@heydari.be>
2020-02-24 14:17:59 +0100

    hashmap merge

--- rust_try/src/hashmap.rs
+++ rust_try/src/hashmap.rs
@@ -13,4 +13,46 @@ pub fn play_hashmap() {
 
     let key = 1;
     assert_eq!(&"Emad44".to_string(), book_reviews.get(&key).unwrap());
+
+
+    // merge 2 hashmaps
+    let mut h1: HashMap<i32,i32> = HashMap::new();
+    let mut h2: HashMap<i32,i32> = HashMap::new();
+
+    h1.insert(1, 3);
+    h1.insert(2, 1);
+    h1.insert(4, 5);
+
+    h2.insert(1, 2);
+    h2.insert(2, 3);
+    h2.insert(6, 1);
+    h2.insert(7, 4);
+
+    let mut h_merged: HashMap<i32,i32> = HashMap::new();
+
+    for (i, j) in h1.iter() {
+        h_merged.insert(i.clone(),j.clone());
+    }
+    for (i, j) in h2.iter() {
+        if h_merged.contains_key(i) {
+            let count = h_merged.get_mut(i).unwrap();
+            *count += *j;
+        } else {
+            h_merged.insert(i.clone(), j.clone());
+        }
+    }
+
+    println!("1 -> {}", h_merged.get(&1).unwrap());
+    println!("2 -> {}", h_merged.get(&2).unwrap());
+    println!("4 -> {}", h_merged.get(&4).unwrap());
+    println!("6 -> {}", h_merged.get(&6).unwrap());
+    println!("7 -> {}", h_merged.get(&7).unwrap());
+    println!("len(): {}", h_merged.len());
+
+    /*assert_eq!(*h_merged.get(&1).unwrap() as usize, 5 as usize);
+    assert_eq!(*h_merged.get(&2).unwrap() as usize, 4 as usize);
+    assert_eq!(*h_merged.get(&4).unwrap() as usize, 5 as usize);
+    assert_eq!(*h_merged.get(&6).unwrap() as usize, 1 as usize);
+    assert_eq!(*h_merged.get(&7).unwrap() as usize, 4 as usize);
+    assert_eq!(h_merged.keys().len(), 5);*/
 }
